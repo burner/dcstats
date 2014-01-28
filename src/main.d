ca54328bb3b96b5145295666cbbd198a1dcbe3f4
Robert burner Schadek <rburners@gmail.com>
2014-01-28 18:40:25 +0100

    dies und das

--- src/main.d
+++ src/main.d
@@ -4,13 +4,61 @@ import std.net.curl : get;
 import std.stdio : writeln;
 import std.xml;
 
-//import std.algorithm;
-import std.range : isInputRange, isOutputRange;
+import std.range : isInputRange, isOutputRange, ElementType;
 import std.typetuple : allSatisfy, TypeTuple;
 
+template allSatisfyBinary(alias F, T...)
+{
+    static if (T.length == 0)
+    {
+        enum allSatisfyBinary = true;
+    }
+    else static if (T.length == 1)
+	{
+        enum allSatisfyBinary = false;
+	}
+    else static if (T.length == 2)
+    {
+        enum allSatisfyBinary = F!(T[0], T[1]);
+    }
+    else
+    {
+        enum allSatisfyBinary = allSatisfyBinary!(F, T[ 0  .. 2]) &&
+			allSatisfyBinary!(F, T[2 .. $]);
+    }
+}
+
+template staticSwitchBind(S, T...)
+{
+    static if (T.length == 0)
+    {
+        alias staticSwitchBind = TypeTuple!();
+    }
+    else static if (T.length == 1)
+    {
+        alias staticSwitchBind = TypeTuple!(T[0], S);
+    }
+    else
+    {
+        alias staticSwitchBind =
+            TypeTuple!(
+                staticSwitchBind!(S, T[ 0  .. $/2]),
+                staticSwitchBind!(S, T[$/2 .. $]));
+    }
+}
+
+pragma(msg, staticSwitchBind!(int, Dummy));
+pragma(msg, allSatisfyBinary!(isOutputRange, staticSwitchBind!(int, Dummy)));
+pragma(msg, staticSwitchBind!(int, Dummy, Dummy));
+pragma(msg, allSatisfyBinary!(isOutputRange, staticSwitchBind!(int, Dummy, Dummy)));
+pragma(msg, staticSwitchBind!(int, Dummy, Dummy, Dummy));
+pragma(msg, allSatisfyBinary!(isOutputRange, staticSwitchBind!(int, Dummy, Dummy, Dummy)));
+
 void distribute(Range1, Ranges...)(Range1 input, Ranges output) 
-	if(isInputRange!Range1)
-	//&& allSatisfy!(isOutputRange, Ranges))
+	if(isInputRange!Range1
+	&& allSatisfyBinary!(isOutputRange, 
+		staticSwitchBind!(ElementType!Range1, Ranges))
+	)
 {
 	foreach(it; input) {
 		foreach(jt; output) {
